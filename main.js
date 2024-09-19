ca1beb8574a4a28e79c1a656d6d630bab732098c
Kamal U G <kamalug@Anandhans-MacBook-Air.local>
2024-09-19 14:16:17 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+
+    // Example: Scroll to section on link click
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
